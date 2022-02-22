33c33a047e28406ad4f0b65499acfab28b473eca
ap.radius <kvark@line-r.ru>
2022-02-22 20:13:32 +0000

    backup2

--- backup.sh
+++ backup.sh
@@ -5,11 +5,13 @@ apache="/etc/apache2"
 radius="/etc/freeradius/"
 dal1="/var/www/html/daloradius/"
 dal2="/var/www/html/daloradius2/"
+fb="/etc/filebeat/filebeat.yml"
 
 tar -zcf nginx.tar.gz $nginx
 tar -zcf apache.tar.gz $apache
 tar -zcf radius.tar.gz $radius
 tar -zcf dal1.tar.gz $dal1
 tar -zcf dal2.tar.gz $dal2
+tar -zcf fb.tar.gz $fb
 
 
