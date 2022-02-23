ea9d60dfcdf030bea0ab7f5b04402ee99fc35afb
ap.radius <kvark@line-r.ru>
2022-02-23 21:51:33 +0000

    backup_2022-02-23_21:51

--- backup.sh
+++ backup.sh
@@ -7,12 +7,12 @@ dal1="/var/www/html/daloradius/"
 dal2="/var/www/html/daloradius2/"
 fb="/etc/filebeat/filebeat.yml"
 
-tar -zcupf nginx.tar.gz $nginx
-tar -zcupf apache.tar.gz $apache
-tar -zcupf radius.tar.gz $radius
-tar -zcupf dal1.tar.gz $dal1
-tar -zcupf dal2.tar.gz $dal2
-tar -zcupf fb.tar.gz $fb
+tar -zcpf nginx.tar.gz $nginx
+tar -zcpf apache.tar.gz $apache
+tar -zcpf radius.tar.gz $radius
+tar -zcpf dal1.tar.gz $dal1
+tar -zcpf dal2.tar.gz $dal2
+tar -zcpf fb.tar.gz $fb
 
 git add -A
 git commit -m 'backup_'$(date +%F_%H:%M)
